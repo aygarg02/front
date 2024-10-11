e912f0e89447a95189d53069ea582d182b5a1d2a
aygarg02 <aygarg02@gmail.com>
2024-10-11 19:07:34 +0530

    Your descriptive commit message here

--- src/App.js
+++ src/App.js
@@ -9,87 +9,103 @@ import Patient from './Application/Patient';
 import Search from './Application/search/Parent';
 import Login from './Application/WrtP/Entry';
 import HR from './Application/HR';
-import Rfmid from './Application/Rfmid'
+import Rfmid from './Application/Rfmid';
 import { useSelector } from "react-redux"; // Import useSelector
+
+// Import images
+import homeImage from './static/public/home.png';
+import teamImage from './static/public/people.png';
+import datasetImage from './static/public/dataset (1).png';
+import arrowImage from './static/public/arrow-right (1).png';
+import toolsImage from './static/public/tools.png';
+import eyeImage from './static/public/eye (1).png';
+import retiImage from './static/public/reti.JPG';
+
 function App() {
   const { isLoggedIn, patientId, name, email } = useSelector((state) => state.auth);
   return (
-
     <div className="App">
       <Router>
-      <div className='parent'>
-      
-        <div className="sidebar">
-          {(isLoggedIn)&&(<div style={{display : 'flex'}}>
-            <img src="reti.JPG" alt="Home" style={{ width: '30px', marginLeft: '10px'}} />
-        <b style={{ width: '30px', marginLeft: '15px', filter: 'brightness(0) invert(1)' }} >{ name }</b>
-        </div>
-        )}
+        <div className='parent'>
+          <div className="sidebar">
+            {(isLoggedIn) && (
+              <div style={{ display: 'flex' }}>
+                <img src={retiImage} alt="Home" style={{ width: '30px', marginLeft: '10px' }} />
+                <b style={{ width: '30px', marginLeft: '15px', filter: 'brightness(0) invert(1)' }}>{name}</b>
+              </div>
+            )}
 
-      
+            <ul>
+              <li>
+                <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+                  Home
+                  <img src={homeImage} alt="Home" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
+                </Link>
+              </li>
+              <li>
+                <Link to="/Team" style={{ display: 'flex', alignItems: 'center' }}>
+                  Team
+                  <img src={teamImage} alt="Team" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
+                </Link>
+              </li>
+              <li className="dataset-dropdown">
+                <Link to="/DataSet" style={{ display: 'flex', alignItems: 'center' }}>
+                  DataSet
+                  <img src={datasetImage} alt="DataSet" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
+                  <img src={arrowImage} alt="Arrow" style={{ width: '20px', marginLeft: '20px', filter: 'brightness(0) invert(1)' }} />
+                </Link>
+                {/* Sub-menu for DataSet */}
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/rfmid" style={{ display: 'flex', alignItems: 'center' }}>
+                      RFMID
+                      <img src={datasetImage} alt="RFMID" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/strave" style={{ display: 'flex', alignItems: 'center' }}>
+                      Strave
+                      <img src={datasetImage} alt="Strave" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
+                    </Link>
+                  </li>
+                  <li><Link to="/some-other" style={{ display: 'flex', alignItems: 'center' }}>Some Other</Link></li>
+                </ul>
+              </li>
+              <li>
+                <Link to="/Login" style={{ display: 'flex', alignItems: 'center' }}>
+                  Patient
+                  <img src={toolsImage} alt="Anotation" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
+                </Link>
+              </li>
+              <li>
+                <Link to="/Search" style={{ display: 'flex', alignItems: 'center' }}>
+                  Search
+                  <img src={toolsImage} alt="Anotation" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
+                </Link>
+              </li>
+              <li>
+                <Link to="/Show" style={{ display: 'flex', alignItems: 'center' }}>
+                  Diagnosis
+                  <img src={eyeImage} alt="DR" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-          <ul>
-            <li>
-              <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
-                Home
-                <img src="home.png" alt="Home" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
-              </Link>
-            </li>
-            <li>
-              <Link to="/Team" style={{ display: 'flex', alignItems: 'center' }}>
-                Team
-                <img src="people.png" alt="Team" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
-              </Link>
-            </li>
-            <li className="dataset-dropdown">
-              <Link to="/DataSet" style={{ display: 'flex', alignItems: 'center' }}>
-                DataSet
-                <img src="dataset (1).png" alt="DataSet" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
-                <img src="arrow-right (1).png" alt="DataSet" style={{ width: '20px', marginLeft: '20px', filter: 'brightness(0) invert(1)' }} />
-              </Link>
-              {/* Sub-menu for DataSet */}
-              <ul className="dropdown-content">
-                <li><Link to="/rfmid"  style={{ display: 'flex', alignItems: 'center' }}>RFMID<img src="dataset (1).png" alt="DataSet" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} /></Link></li>
-                <li><Link to="/strave"  style={{ display: 'flex', alignItems: 'center' }}>Strave<img src="dataset (1).png" alt="DataSet" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} /></Link></li>
-                <li><Link to="/some-other"  style={{ display: 'flex', alignItems: 'center' }}>Some Other</Link></li>
-              </ul>
-            </li>
-            <li>
-              <Link to="/Login" style={{ display: 'flex', alignItems: 'center' }}>
-                Patient
-                <img src="tools.png" alt="Anotation" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
-              </Link>
-            </li>
-            <li>
-              <Link to="/Search" style={{ display: 'flex', alignItems: 'center' }}>
-                Search
-                <img src="tools.png" alt="Anotation" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
-              </Link>
-            </li>
-            <li>
-              <Link to="/Show" style={{ display: 'flex', alignItems: 'center' }}>
-                Diagnosis
-                <img src="eye (1).png" alt="DR" style={{ width: '20px', marginLeft: '10px', filter: 'brightness(0) invert(1)' }} />
-              </Link>
-            </li>
-         
-          </ul>
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/Team" element={<Team />} />
+              <Route path="/rfmid" element={<Rfmid />} />
+              <Route path="/DataSet" element={<DataSet />} />
+              <Route path="/Anotation" element={<Anotation />} />
+              <Route path="/Show" element={<Show />} />
+              <Route path="/Login" element={<Login />} />
+              <Route path="/Search" element={<Search />} />
+              <Route path="/DR" element={<HR />} />
+            </Routes>
+          </div>
         </div>
-
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Team" element={<Team />} />
-            <Route path="/rfmid" element={<Rfmid />} />
-            <Route path="/DataSet" element={<DataSet />} />
-            <Route path="/Anotation" element={<Anotation />} />
-            <Route path="/Show" element={<Show />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Search" element={<Search />} />
-            <Route path="/DR" element={<HR />} />
-          </Routes>
-        </div>
-      </div>
       </Router>
     </div>
   );
--- src/Application/Team.js
+++ src/Application/Team.js
@@ -1,58 +1,64 @@
 import React from 'react';
 import './Team.css'; // Ensure you import the CSS file for styles
 
+// Import images
+import member1 from '../static/public/image.png';
+import member2 from '../static/public/c.png';
+import member3 from '../static/public/8.png';
+import member4 from '../static/public/Tony.png';
+
 function Team() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>Member Details</h1> {/* Centering the text */}
 
-   {/* Team Member 1 */}
-<div className="team-member">
-  <div className="member-photo">
-    <img src='/image.png' alt="Tony" /> {/* Image path */}
-  </div>
-  <div className="member-details">
-    <h2>Dr. Meera Dhabu</h2> {/* Main heading: Member Name */}
-    <h3 style={{ margin: '5px 0', color: 'lightgray' }}>ASSOCIATE PROFESSOR</h3> {/* Subheading: Position */}
-    <h3  style={{ margin: '5px 0', color: 'lightgray' }}>Speciality: Data Mining, Soft Computing, Network Security, Smart Grid.</h3> {/* Additional details */}
-  </div>
-</div>
-<div className="team-member">
-  <div className="member-photo">
-    <img src='/c.png' alt="Tony" /> {/* Image path */}
-  </div>
-  <div className="member-details">
-    <h2>Dr. Ashish S. Tiwari </h2> {/* Main heading: Member Name */}
-    <h3 style={{ margin: '5px 0', color: 'lightgray' }}>ASSISTANT PROFESSOR</h3> {/* Subheading: Position */}
-    <h3  style={{ margin: '5px 0', color: 'lightgray' }}>Speciality: Mobile Communication, Information Security, Operating Systems</h3> {/* Additional details */}
-  </div>
-</div>
-<div className="team-member">
-  <div className="member-photo">
-    <img src='/8.png' alt="Tony" /> {/* Image path */}
-  </div>
-  <div className="member-details">
-    <h2>Ayush Garg</h2> {/* Main heading: Member Name */}
-    <h3 style={{ margin: '5px 0', color: 'lightgray' }}>Software Engineer</h3> {/* Subheading: Position */}
-    <h3  style={{ margin: '5px 0', color: 'lightgray' }}>Details: This section can contain more information about Ayush.</h3> {/* Additional details */}
-  </div>
-</div>
-<div className="team-member">
-  <div className="member-photo">
-    <img src='/Tony.png' alt="Tony" /> {/* Image path */}
-  </div>
-  <div className="member-details">
-    <h2>Suvendu </h2> {/* Main heading: Member Name */}
-    <h3 style={{ margin: '5px 0', color: 'lightgray' }}>Software Engineer</h3> {/* Subheading: Position */}
-    <h3  style={{ margin: '5px 0', color: 'lightgray' }}>Details: This section can contain more information about Ayush.</h3> {/* Additional details */}
-  </div>
-</div>
+      {/* Team Member 1 */}
+      <div className="team-member">
+        <div className="member-photo">
+          <img src={member1} alt="Dr. Meera Dhabu" /> {/* Image path */}
+        </div>
+        <div className="member-details">
+          <h2>Dr. Meera Dhabu</h2> {/* Main heading: Member Name */}
+          <h3 style={{ margin: '5px 0', color: 'lightgray' }}>ASSOCIATE PROFESSOR</h3> {/* Subheading: Position */}
+          <h3 style={{ margin: '5px 0', color: 'lightgray' }}>Speciality: Data Mining, Soft Computing, Network Security, Smart Grid.</h3> {/* Additional details */}
+        </div>
+      </div>
 
+      {/* Team Member 2 */}
+      <div className="team-member">
+        <div className="member-photo">
+          <img src={member2} alt="Dr. Ashish S. Tiwari" /> {/* Image path */}
+        </div>
+        <div className="member-details">
+          <h2>Dr. Ashish S. Tiwari</h2> {/* Main heading: Member Name */}
+          <h3 style={{ margin: '5px 0', color: 'lightgray' }}>ASSISTANT PROFESSOR</h3> {/* Subheading: Position */}
+          <h3 style={{ margin: '5px 0', color: 'lightgray' }}>Speciality: Mobile Communication, Information Security, Operating Systems</h3> {/* Additional details */}
+        </div>
+      </div>
 
-      
+      {/* Team Member 3 */}
+      <div className="team-member">
+        <div className="member-photo">
+          <img src={member3} alt="Ayush Garg" /> {/* Image path */}
+        </div>
+        <div className="member-details">
+          <h2>Ayush Garg</h2> {/* Main heading: Member Name */}
+          <h3 style={{ margin: '5px 0', color: 'lightgray' }}>Software Engineer</h3> {/* Subheading: Position */}
+          <h3 style={{ margin: '5px 0', color: 'lightgray' }}>Details: This section can contain more information about Ayush.</h3> {/* Additional details */}
+        </div>
+      </div>
 
-
-    
+      {/* Team Member 4 */}
+      <div className="team-member">
+        <div className="member-photo">
+          <img src={member4} alt="Suvendu" /> {/* Image path */}
+        </div>
+        <div className="member-details">
+          <h2>Suvendu</h2> {/* Main heading: Member Name */}
+          <h3 style={{ margin: '5px 0', color: 'lightgray' }}>Software Engineer</h3> {/* Subheading: Position */}
+          <h3 style={{ margin: '5px 0', color: 'lightgray' }}>Details: This section can contain more information about Suvendu.</h3> {/* Additional details */}
+        </div>
+      </div>
 
       {/* Add more team members as needed */}
     </>
